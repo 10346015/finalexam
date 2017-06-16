eeff84eb347b593e17f7725b6e6250f63bfab8bf
Sherly <10346015@ntub.edu.tw>
2017-06-16 15:19:00 +0800

    do classify

--- ntub/ntub/app/helpers/students_helper.rb
+++ ntub/ntub/app/helpers/students_helper.rb
@@ -1,2 +1,22 @@
 module StudentsHelper
+  def do_classify(student)
+    if student.grade >= 90
+      student.classify = "S級"
+    else
+      if student.grade >= 80
+       student.classify = "A級"
+      else
+        if student.grade >= 70
+        student.classify = "B級"
+        else
+          if student.grade >= 60
+         student.classify = "C級"
+         else
+           student.classify = "D級"
+          end
+        end
+      end
+    end  
+      
+  end
 end
